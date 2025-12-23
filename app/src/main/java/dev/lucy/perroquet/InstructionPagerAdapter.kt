72717829c6e25ace82d69a1648319530c04372c2
andreaesantos <andrea98@me.com>
2025-12-23 19:45:26 +0100

    added perroquet icon

---
+++ app/src/main/java/dev/lucy/perroquet/InstructionPagerAdapter.kt
@@ -0,0 +1,29 @@
+//package dev.andrea.perroquet
+//
+//import android.view.LayoutInflater
+//import android.view.View
+//import android.view.ViewGroup
+//import android.widget.TextView
+//import androidx.recyclerview.widget.RecyclerView
+//
+//class InstructionPagerAdapter(
+//    private val instructionPages: List<InstructionActivity.InstructionPage>
+//) : RecyclerView.Adapter<InstructionPagerAdapter.InstructionViewHolder>() {
+//
+//    class InstructionViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+//        val textView: TextView = view.findViewById(R.id.instructionTextView)
+//    }
+//
+//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): InstructionViewHolder {
+//        val view = LayoutInflater.from(parent.context)
+//            .inflate(R.layout.instruction_page_item, parent, false)
+//        return InstructionViewHolder(view)
+//    }
+//
+//    override fun onBindViewHolder(holder: InstructionViewHolder, position: Int) {
+//        val page = instructionPages[position]
+//        holder.textView.text = page.text
+//    }
+//
+//    override fun getItemCount(): Int = instructionPages.size
+//}
